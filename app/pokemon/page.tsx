f6490c9d0e508573bd8be7ebc2137135a34979cd
zaidahmed <zaid.42005@gmail.com>
2023-09-26 10:20:11 +0400

    grey back and bacground image commented

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -7,7 +7,7 @@ const Pokemon = () => {
     const [search,setSearch] = useState('')
     const [pokeFrontUrl, setPokeFrontUrl] = useState('') 
     const [pokeBackUrl, setPokeBackUrl] = useState('')
-    const [pokeTypeUrl,setPokeTypeUrl] = useState('')
+    const [pokeTypeUrl,setPokeTypeUrl] = useState([])
     const [showFrontImage, setShowFrontImage] = useState(true);
     const submit = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault()
@@ -26,16 +26,18 @@ const Pokemon = () => {
       };
 
   return (
-    <div className='min-h-screen bg-red-500 flex flex-col justify-center items-center gap-4'>
-        <form className='flex flex-col items-center justify-center gap-4' onSubmit ={(e)=>submit(e)}>
+    <div className='min-h-screen bg-slate-500 flex flex-col justify-center items-center gap-4'>
+        <form className=   'flex flex-col items-center justify-center gap-4' onSubmit ={(e)=>submit(e)}>
             <input placeholder='Search...' className= "text-lime-900 bg-neutral-200" value={search} onChange={(e)=> setSearch(e.target.value)}/>
             <button className= 'bg-amber-100 text-black px-2 py-1 rounded-lg' type='submit'> Ooooo</button>
         </form>
-        
-        
-        <img className="h-20 w-20" src={showFrontImage? pokeFrontUrl: pokeBackUrl} alt="Pokemon" />
 
-        <button className="bg-amber-100 text-black px-2 py-1 rounded-lg" onClick={toggleImage}>switch view</button>
+        {/* <img src = 'https://wallpapercave.com/wp/wp3170174.jpg' /> */}
+        
+        
+        <img className="h-35 w-40" src={showFrontImage? pokeFrontUrl: pokeBackUrl} alt="Pokemon" />
+
+        <button className="bg-amber-100 text-black px-2 py-1 rounded-lg" onClick={toggleImage}>switch</button>
         
         <h1 className='rounded-lg bg-slate-50 text-black px-4 '>{pokeTypeUrl} </h1>
     </div>
